6e58784eeb3cfd4d346d1fc42daa02cfbc2ef991
talitabac@gmail.com <talitabac@gmail.com@af0c2be1-2123-fc44-0d32-0a0e62ed318f>
2011-05-28 15:16:09 +0000

    começando os testes da classe queue

--- ProjetoLEDA/src/testes/TestQueueImpl.java
+++ ProjetoLEDA/src/testes/TestQueueImpl.java
@@ -1,6 +1,45 @@
 package testes;
 
+import eda.linearADT.QueueImpl;
+import eda.linearADT.StackImpl;
+import eda.util.ADTOverflowException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Universidade Federal de Campina Grande
+ * Projetos de Laboratorio de Estrutura de Dados
+ * 
+ * Equipe:  Layse Sobreira
+ * 			Renan Pinto
+ * 			Rodolfo Viana
+ * 			Talita Lobo
+ * 
+ * 	Implementacao das Estruturas Lineares
+ * 		Lista Dupla, Lista Simples, Fila e Pilha
+ * 			Testes da Classe QueueImpl.java
+ */
+
 
 public class TestQueueImpl {
+	QueueImpl<String> q1;
+	
+	@Before
+	public void instancia() {
+		q1 = new QueueImpl<String>();
+	}
+	
+	@Test
+	public void testFilaVazia() {
+		Assert.assertTrue(q1.isEmpty());
+		Assert.assertEquals("Array deve ser vazio", q1.toString(), "[]");
+	}
+
+	@Test
+	public void testEqueue() {
+		
+	}
+	
 
 }
