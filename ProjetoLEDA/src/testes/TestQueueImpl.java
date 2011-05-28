d63b4ef0e5575339a4f581c0013f3092dd18b9a8
talitabac@gmail.com <talitabac@gmail.com@af0c2be1-2123-fc44-0d32-0a0e62ed318f>
2011-05-28 20:04:35 +0000

    testes bjs

--- ProjetoLEDA/src/testes/TestQueueImpl.java
+++ ProjetoLEDA/src/testes/TestQueueImpl.java
@@ -43,21 +43,61 @@ public class TestQueueImpl {
 		Assert.assertTrue(q1.isEmpty());
 		Assert.assertEquals("Array deve ser vazio", q1.toString(), "[]");
 		
+		System.out.println("Teste Enqueue");
 		q1.enqueue("elemento1");q1.enqueue("elemento2");q1.enqueue("elemento3");
+		System.out.println(q1.toString());
 		q1.enqueue("elemento4");q1.enqueue("elemento5");q1.enqueue("elemento6");
+		System.out.println(q1.toString());
 		q1.enqueue("elemento7");q1.enqueue("elemento8");q1.enqueue("elemento9");
-		q1.enqueue("elemento10");
-		//System.out.println(q1.toString());
+		System.out.println(q1.toString());
+		System.out.println("\n"); 
 		
-		Assert.assertEquals("Array tem 10 elementos",  "[elemento1, elemento2, elemento3, elemento4, elemento5, elemento6, elemento7, elemento8, elemento9, elemento10]", q1.toString());
+		Assert.assertEquals("Array tem 9 elementos",  "[elemento1, elemento2, elemento3, elemento4, elemento5, elemento6, elemento7, elemento8, elemento9]", q1.toString());
 	}
 	
 	@Test
-	public void testDequeue() throws ADTUnderflowException {
-		//q1.dequeue();
-		//q1.dequeue();
+	public void testDequeue() throws ADTUnderflowException, ADTOverflowException {
+		q1.enqueue("elemento1");
+		q1.enqueue("elemento2");
+		q1.enqueue("elemento3");
+		q1.enqueue("elemento4");
+		q1.enqueue("elemento5");
+		q1.enqueue("elemento6");
+		q1.enqueue("elemento7");
+		
+		System.out.println("Teste Dequeue");
+		System.out.println(q1.toString());
+		
+		q1.dequeue();
+		System.out.println(q1.toString());
+		q1.dequeue();
+		System.out.println(q1.toString());
+		q1.dequeue();
+		System.out.println(q1.toString());
+		q1.dequeue();
+		System.out.println(q1.toString());
+		System.out.println("\n"); 
+		
+	}
+	
+	@Test
+	public void testHead() throws ADTOverflowException, ADTUnderflowException {
+		q1.enqueue("elemento5");
+		q1.enqueue("elemento6");
+		q1.enqueue("elemento7");
+		
+		System.out.println("Teste Head");
+		System.out.print("Head 1: ");
+		System.out.println(q1.head());
+		
+		q1.dequeue();
+		System.out.print("Head 2: ");
+		System.out.println(q1.head());
+		
+		q1.dequeue();
+		System.out.print("Head 3: ");
+		System.out.println(q1.head());
 	}
 	
 	
-
 }
