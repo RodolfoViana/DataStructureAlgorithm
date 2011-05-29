cb6bd75d3bd2db6571649e8d7f1bd62caae208a6
rodolfodelimaviana <rodolfodelimaviana@af0c2be1-2123-fc44-0d32-0a0e62ed318f>
2011-05-29 02:31:34 +0000

    Rodolfo =D

---
+++ ProjetoLEDA/src/testes/TestDoubleLinkedListNonRecursiveImpl.java
@@ -0,0 +1,32 @@
+package testes;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import eda.linearADT.DoubleLinkedListNonRecursiveImpl;
+import eda.util.ADTOverflowException;
+
+public class TestDoubleLinkedListNonRecursiveImpl {
+	
+	DoubleLinkedListNonRecursiveImpl<Integer> linkedList;
+	
+	@Before public void construtor(){
+		linkedList = new DoubleLinkedListNonRecursiveImpl<Integer>();
+	}
+	
+	@Test public void isEmpty(){
+		Assert.assertTrue(linkedList.isEmpty());
+	}
+	
+	@Test public void insert() throws ADTOverflowException{
+		Assert.assertTrue(linkedList.isEmpty());
+		
+		linkedList.insert(10);
+		Assert.assertEquals("Erro no insert", 1, linkedList.size());
+		linkedList.insert(15);
+		Assert.assertEquals("Erro no insert", 2, linkedList.size());		
+	}
+	
+
+}
--- ProjetoLEDA/src/testes/TestDoubleLinkedListRecursiveImpl.java
+++ ProjetoLEDA/src/testes/TestDoubleLinkedListRecursiveImpl.java
@@ -3,6 +3,7 @@ package testes;
 import eda.linearADT.DoubleLinkedListRecursiveImpl;
 import eda.util.ADTNoSuchElement;
 import eda.util.ADTOverflowException;
+import eda.util.ADTUnderflowException;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -70,5 +71,112 @@ public class TestDoubleLinkedListRecursiveImpl {
 		Assert.assertTrue("Revert errado", linkedList2.equals(linkedList.revert()) );
 		
 	}
+	
+	@Test public void isHead() throws ADTOverflowException{
+		Assert.assertFalse(linkedList.isHead());
+		linkedList.insert(10);
+		Assert.assertTrue(linkedList.isHead());
+	}
+	
+	@Test public void addFirst() throws ADTOverflowException{
+		
+		linkedList.insert(2);
+		linkedList.insert(3);
+		linkedList.insert(4);
+		
+		Integer[] aux = new Integer[3];
+		aux[0] = 2;
+		aux[1] = 3;
+		aux[2] = 4;
+		
+		
+		
+		
+		Assert.assertArrayEquals(aux, linkedList.toArray());
+		
+//		linkedList.addFirst(10);
+//		
+//		aux = new Integer[4];
+//		aux[0] = 10;
+//		aux[1] = 2;
+//		aux[2] = 3;
+//		aux[3] = 4;
+//		
+//		Assert.assertArrayEquals(aux, linkedList.toArray());
+
+//		
+//		DoubleLinkedListRecursiveImpl<Integer> linkedList2 = new DoubleLinkedListRecursiveImpl<Integer>();
+//		linkedList2.insert(2);
+//		linkedList2.insert(3);
+//		linkedList2.insert(4);
+//		
+//		
+		
+	}
+	
+	@Test public void addLast() throws ADTOverflowException{
+		
+		linkedList.insert(2);
+		linkedList.insert(3);
+		linkedList.insert(4);
+		
+		Integer[] aux = new Integer[3];
+		aux[0] = 2;
+		aux[1] = 3;
+		aux[2] = 4;
+		
+		Assert.assertArrayEquals(aux, linkedList.toArray());
+		
+		linkedList.addLast(10);
+		
+		aux = new Integer[4];
+		aux[0] = 2;
+		aux[1] = 3;
+		aux[2] = 4;
+		aux[3] = 10;
+		
+		Assert.assertArrayEquals(aux, linkedList.toArray());
+	}
+	
+	@Test public void removeLast() throws ADTOverflowException, ADTUnderflowException{
+		
+		linkedList.insert(2);
+		linkedList.insert(3);
+		linkedList.insert(4);
+		
+		Assert.assertEquals("Erro ao adicionar", 3, linkedList.size());
+		
+		linkedList.removeLast();
+		
+		Assert.assertEquals("Erro ao remover", 2, linkedList.size());
+		
+		Integer[] aux = new Integer[2];
+		aux[0] = 2;
+		aux[1] = 3;
+		
+		Assert.assertArrayEquals(aux, linkedList.toArray());
+	}
+	
+	@Test public void removeFirt() throws ADTOverflowException, ADTUnderflowException{
+		linkedList.insert(2);
+		linkedList.insert(3);
+		linkedList.insert(4);
+		
+		Assert.assertEquals("Erro ao adicionar", 3, linkedList.size());
+		
+		linkedList.removeFirst();
+		
+		Assert.assertEquals("Erro ao remover", 2, linkedList.size());
+		
+
+		Integer[] aux = new Integer[2];
+		aux[0] = 3;
+		aux[1] = 4;
+		
+		Assert.assertArrayEquals(aux, linkedList.toArray());
+	}
+	
+	
+	
 
 }
